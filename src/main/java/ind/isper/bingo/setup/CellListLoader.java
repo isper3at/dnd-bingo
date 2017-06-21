d91577556e3c7a6e302c2435ff0280c356ad8494
isper3at <smith3at@gmail.com>
2017-06-21 13:44:24 -0400

    Added pom to maven-fy the project

--- src/main/java/ind/isper/bingo/setup/CellListLoader.java
+++ src/main/java/ind/isper/bingo/setup/CellListLoader.java
@@ -1,5 +1,30 @@
 package ind.isper.bingo.setup;
 
-public class CellListLoader {
+import java.nio.file.Path;
+import java.util.Iterator;
+
+/**
+ * Loads the possible bingo cell list into an iterator.
+ * 
+ * The next() result will be randomly selected for ease
+ * of generation of bingo sheet.  The iterator will
+ * be lazily loaded, after a full load reset() will
+ * allow all possible cells to be used again.
+ */
+public class CellListLoader implements Iterator<String> {
+	public CellListLoader(final Path cellList) {
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
